458497db67a920a1c680124b8c689fc2305896f8
dleorud72 <wertlkjh0987@gmail.com>
2024-11-20 13:37:16 +0900

    임포트 추가

--- tetris_chatater_project/src/tetris_chatater/chatater.java
+++ tetris_chatater_project/src/tetris_chatater/chatater.java
@@ -1,5 +1,12 @@
 package tetris_chatater;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.*;
+
 public class chatater {
 
 }
